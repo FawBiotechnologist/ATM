10d7375063ddc6d3041a2833fe7d0746b01e7770
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-05-06 20:28:04 +0200

    finished test, fixed some bugs

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Currency;
+import java.util.List;
 import java.util.Locale;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,12 +24,14 @@ class ATMachineTest {
 	Nie jestem pewien czy to poprawny zapis z kodu wynika, ze obslugujemy tylko polska walute ?(class Banknote)
 	w przypadku uruchomienia tego polecenia z innym locale kod moglby sie wysypac.
 	*/
+	private final int DEFAULT_VALID_AMOUNT_OF_MONEY = 2000;
+	private final Withdrawal EXPECTED_WITHDRAWAL = Withdrawal.create(List.of(BanknotesPack.create(4, Banknote.PL_500)));
 	Currency validCurrency = Currency.getInstance(Locale.getDefault());
 	//Currency invalidCurrency;
 	ATMachine testedATMMachine;
 	PinCode properPinCode;
 	Card validCard;
-	Money validAmmount;
+	Money validAmount;
 
 
 	@BeforeEach
@@ -36,7 +40,7 @@ class ATMachineTest {
 		testedATMMachine.setDeposit(createDefaultDeposit(10));
 		properPinCode = PinCode.createPIN(1, 2, 3, 4);
 		validCard = Card.create("1");
-		validAmmount = new Money(2000, validCurrency);
+		validAmount = new Money(DEFAULT_VALID_AMOUNT_OF_MONEY, validCurrency);
 	}
 
 	@Test
@@ -45,10 +49,9 @@ class ATMachineTest {
 	}
 
 	@Test
-	void withrawingProperAmmountExpectingSuccesWithNoErrors() throws ATMOperationException {
-		Withdrawal withdrewMoney = testedATMMachine.withdraw(properPinCode, validCard, validAmmount);
-		System.out.println(withdrewMoney);
-
+	void withdrawingProperAmountExpectingSuccessWithNoErrors() throws ATMOperationException {
+		Withdrawal withdrewMoney = testedATMMachine.withdraw(properPinCode, validCard, validAmount);
+		Assertions.assertEquals(EXPECTED_WITHDRAWAL, withdrewMoney);
 	}
 
 	private MoneyDeposit createDefaultDeposit(int countForEachPack) {
