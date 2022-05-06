9dd15f69804c28507df9300b1384c86116b7ae24
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-05-06 18:15:27 +0200

    fixing typo in enum type, setting baseline for tests

--- src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/atm/ATMachineTest.java
@@ -2,18 +2,31 @@ package edu.iis.mto.testreactor.atm;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.atm.bank.Bank;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Currency;
+
+@ExtendWith(MockitoExtension.class)
 class ATMachineTest {
+	@Mock
+	Bank bankMock;
+	Currency validCurrency;
+	//Currency invalidCurrency;
 
-    @BeforeEach
-    void setUp() throws Exception {}
 
-    @Test
-    void itCompiles() {
-        assertThat(true, Matchers.equalTo(true));
-    }
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void itCompiles() {
+		assertThat(true, Matchers.equalTo(true));
+	}
 
 }
